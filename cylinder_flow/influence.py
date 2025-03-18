fixed definition of normal vector to the panel in geometry.py, fixed calculation of tangential velocity in solver.py

cylinder_flow/influence.py
@@ -9,12 +9,12 @@ def compute_influence_coefficients(N, x, y, x_mid, y_mid, n_vect, t_vect):
             dx = x_mid[i] - x[j]
             dy = y_mid[i] - y[j]
             r = np.sqrt(dx**2 + dy**2)
-            t_induced = np.array([dx / r, dy / r])
-            n_induced = np.array([dy/r , -dx /r])
+            t_induced = np.array([dx , dy ])/r
+            n_induced = np.array([dy , -dx ])/r
             # print('|n_vect|, should give one:', np.sqrt(n_induced[0]**2 + n_induced[1]**2))
             # assert np.dot(t_induced, n_induced) == 0
-            AN[i, j] = -np.dot(n_vect[i], n_induced) / (2 * np.pi * r)
-            AN_3d[:,j,i] = -n_induced/(2*np.pi*r)
+            AN[i, j] =  - np.dot(n_vect[i], n_induced) / (2 * np.pi * r)
+            AN_3d[:,j,i] = - n_induced/(2*np.pi*r)
     AN[-1, :] = 0
     AN[-1, 0] = 1
     AN[-1, -1] = 1

cylinder_flow/solver.py
@@ -35,14 +35,12 @@ def compute_velocity_and_pressure_3d(N, V_inf, theta_mid, AN, AN_3d, Gamma,AOA,t
     V_inf  * np.array([[np.cos(AOA)], [np.sin(AOA)]])
     )    
     #add the velocity correction to vel_vect (shape (2, np) + (2, 1))
-    vel_vect -= velocity_correction 
+    vel_vect += velocity_correction 
     
     print('Shape of vel_vect after adding velocity correction:', np.shape(vel_vect))
-    # print(vel_vect)
-    # Calculate the velocity magnitude
-    Vt = np.linalg.norm(vel_vect, axis=0)# This gives a vector of shape (np), magnitude of Vel
 
-    Vt = vel_vect[1,:] # taking only the component in y
+    # Compute the tangletial velocity with the tangential vector (cos(theta),sin(theta))
+    Vt =  vel_vect[0,:]*np.cos(theta_mid) + vel_vect[1,:]*np.sin(theta_mid) # 
 
     # Calculate Cp (coefficient of pressure)
     Cp = 1 - (Vt/ V_inf) ** 2